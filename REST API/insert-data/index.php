b3bc4acd41bea47389b5cdc216e7ec5d886032c5
Palmaaa <36799395+Palmaaa@users.noreply.github.com>
2019-05-03 14:59:15 -0300

    Correção para o fuso horário brasileiro

--- REST API/insert-data/index.php
+++ REST API/insert-data/index.php
@@ -1,10 +1,10 @@
 <?php
 
 include '../SQLMethods.php';
-
+date_default_timezone_set("America/Sao_Paulo");
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_GET['data']) && is_numeric($_GET['data']) && isset($_GET['key']) && $_GET['key'] == 'ic2018') {
-        SQLMethods::insert("INSERT INTO DADOS (concentracao, data_hora) VALUES ({$_GET['data']}, NOW())");
+        SQLMethods::insert("INSERT INTO DADOS (concentracao, data_hora) VALUES ({$_GET['data']}, '".date('Y-m-d H:i:s', time())."')");
         http_response_code(200); // OK
     } else {
         http_response_code(400); // Erro de sintaxe
