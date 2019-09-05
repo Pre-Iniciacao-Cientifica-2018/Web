35392edfabdf92227c725b36796c71e8dc7e9b65
Palmaaa <36799395+Palmaaa@users.noreply.github.com>
2019-09-05 15:55:51 -0300

    Adição da página tempo-real

---
+++ REST API/tempo-real/index.php
@@ -0,0 +1,15 @@
+﻿<?php
+include '../SQLMethods.php';
+
+$data = SQLMethods::select("SELECT concentracao FROM DADOS ORDER BY data_hora desc LIMIT 1");
+$json = array();
+$json["data"] = array();
+if ($data != null) {
+    $json["data"] = ['tempo-real' => $data[0][0]];
+} else {
+    $json["data"] = ['erro' => 'Não foi possível acessar o banco de dados'];
+    http_response_code(500);
+}
+header_remove();
+header('Content-Type: application/json');
+echo json_encode($json);
